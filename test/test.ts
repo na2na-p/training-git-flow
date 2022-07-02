197f1cccd9da9b7bdec2e338d50c7e8c3ef5d220
na2na <49822810+na2na-p@users.noreply.github.com>
2022-07-02 07:35:33 +0000

    Add test

--- test/test.ts
+++ test/test.ts
@@ -5,17 +5,16 @@ import { funcA } from '../built/modules/funcA.js';
 import { funcB } from '../built/modules/funcB.js';
 import { funcC } from '../built/modules/funcC.js';
 
-describe('モジュールのテスト', () => {
+// mochaを利用したテストコード
+describe('モジュール群のテスト', () => {
 	it('funcAのテスト', () => {
-		const inputStr = '迫　竜乃介';
-		expect(funcA(inputStr)).to.equal('名前: 迫　竜乃介');
+		const inputStr = '山田太郎';
+		expect(funcA(inputStr)).to.equal('名前: 山田太郎');
 	});
 	it('funcBのテスト', () => {
-		const inputStr = '22';
-		expect(funcB(inputStr)).to.equal('年齢: 22歳');
+		expect(funcB('20')).to.equal('年齢: 20歳');
 	});
 	it('funcCのテスト', () => {
-		const inputStr = '2000-06-17';
-		expect(funcC(inputStr)).to.equal('生年月日: 2000-06-17');
+		expect(funcC('2000-01-01')).to.equal('生年月日: 2000-01-01');
 	});
 });
